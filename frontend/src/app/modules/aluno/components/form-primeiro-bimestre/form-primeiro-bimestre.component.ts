baa2c545e0db80e76c785883fd5fd374efec902a
Jhonathan Ribeiro <jhonathan.ribeiro@ids.inf.br>
2021-12-06 13:08:33 -0300

    Adicionado toast messages

--- frontend/src/app/modules/aluno/components/form-primeiro-bimestre/form-primeiro-bimestre.component.ts
+++ frontend/src/app/modules/aluno/components/form-primeiro-bimestre/form-primeiro-bimestre.component.ts
@@ -3,23 +3,33 @@ import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlunoService } from '../../aluno.service';
 import { paramsPrimeiroBimestre } from '../../helpers/params';
+import { Message } from 'primeng/components/common/message';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-form-primeiro-bimestre',
-  templateUrl: './form-primeiro-bimestre.component.html'
+  templateUrl: './form-primeiro-bimestre.component.html',
+  providers: [MessageService]
 })
 export class FormPrimeiroBimestreComponent implements OnInit {
   formPrimeiroBimestre: FormGroup;
- 
+
+  msgs1: Message[];
+
   constructor(
     private fb: FormBuilder,
     private api: AlunoService,
-    private route: ActivatedRoute
-    ) { }
+    private route: ActivatedRoute,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.formularioPrimeiroBimestre();
     this.populaDadosFormulario();
+  }
+
+  public showError() {
+    this.messageService.add({ severity: 'error', summary: 'Server error', detail: '500 error' });
   }
 
   public populaDadosFormulario(): void {
@@ -31,9 +41,9 @@ export class FormPrimeiroBimestreComponent implements OnInit {
         status: dados.status,
         situacao: dados.situacao,
         primeira_nota_primeiro_bimestre: [dados.bimestres[0].n1, Validators.required],
-        segunda_nota_primeiro_bimestre: [dados.bimestres[0].n2,Validators.required],
-        terceira_nota_primeiro_bimestre: [dados.bimestres[0].n3,Validators.required],
-        quarta_nota_primeiro_bimestre: [dados.bimestres[0].n4,Validators.required],
+        segunda_nota_primeiro_bimestre: [dados.bimestres[0].n2, Validators.required],
+        terceira_nota_primeiro_bimestre: [dados.bimestres[0].n3, Validators.required],
+        quarta_nota_primeiro_bimestre: [dados.bimestres[0].n4, Validators.required],
         faltas_primeiro_bimestre: [dados.bimestres[0].faltas, Validators.required]
       });
     });
@@ -54,13 +64,14 @@ export class FormPrimeiroBimestreComponent implements OnInit {
     this.api.getAlunoById(id).subscribe(dados => {
       try {
         const params = paramsPrimeiroBimestre(dados, this.formPrimeiroBimestre);
-        if(params && params !== null) {
-          this.api.atualizaBimestre(id, params).subscribe(dados =>{console.log(dados)});
+        if (params && params !== null) {
+          this.api.atualizaBimestre(id, params).subscribe(dados => { console.log(dados) });
+          this.messageService.add({ severity: 'success', summary: '', detail: '' });
         } else {
           throw new Error('Não foi possível atualizar as notas.');
         }
       } catch (error) {
-        console.log(error);
+        this.messageService.add({ severity: 'error', summary: 'Server error', detail: `${error.message}` });
       }
     });
   }
